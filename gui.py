fa7a892b047f7dc2f5ace9e6d1ebd4542df23b22
Maxima <maxime.arthaud@gmail.com>
2013-02-27 22:46:41 +0100

    GUI reorganized.

--- gui.py
+++ gui.py
@@ -21,81 +21,101 @@ def wrapper(fun):
             curses.endwin()
     return wrapped
 
-class Window(object):
-    def __init__(self, screen):
-        self.screen = screen
+class Window:
+    def __init__(self, x, y, width, height):
         self._win = curses.newwin(0, 0)
-        self.reset_size()
+        self.resize(x, y, width, height)
 
-    def refresh(self):
+    def _refresh(self):
+        ''' refresh the window. all things drawn will be printed. '''
         self._win.refresh()
 
-    def reset_size(self):
-        pass
+    def _erase(self):
+        ''' clear the window. the next refresh, the window will be empty. '''
+        self._win.erase()
+
+    def resize(self, x, y, width, height):
+        ''' resize the window, and redraw it. '''
+        self._win.resize(height, width)
+        self._win.mvwin(y, x)
+        self.redraw()
+
+    def redraw(self):
+        ''' redraw the window. '''
+        self._refresh()
 
     def key_event(self, key):
+        ''' manage a keyboard event. '''
         pass
 
 class UserInput(Window):
-    def __init__(self, screen, client):
+    PROMPT = '[%s] # '
+
+    def __init__(self, client, x, y, width, height):
         self.client = client
-        self._input = u''
-        Window.__init__(self, screen)
+        self._input = str()
+        self._cursor = 0
+        Window.__init__(self, x, y, width, height)
 
-    def reset_size(self):
-        y, x = self.screen.getmaxyx()
-        self._win.resize(1, x)
-        self._win.mvwin(y-1, 0)
+    def get_prompt(self):
+        return UserInput.PROMPT % self.client.username
 
-        self._win.erase()
-        self._win.addstr('[%s] # %s' % (self.client.username, self._input))
+    def redraw(self):
+        self._erase()
+        self._win.addstr(self.get_prompt())
+        self._win.addstr(self._input)
+        self.redraw_cursor()
+        self._refresh()
+
+    def redraw_cursor(self):
+        self._win.move(0, len(self.get_prompt()) + self._cursor)
+        self._refresh()
 
     def key_event(self, key):
-        self._win.addstr(chr(key))
-        self.refresh()
+        if key >= 0 and key <= 255:
+            self._win.addstr(chr(key))
+            self._cursor += 1
+            self._refresh()
             
 class Chat(Window):
-    def __init__(self, screen, messages):
+    def __init__(self, messages, redraw_cursor, x, y, width, height):
         self._messages = messages
-        self._index = 0
+        self.redraw_cursor = redraw_cursor
+        self._scroll = 0
         self._lines = []
-        Window.__init__(self, screen)
-        self._win.leaveok(True)
+        Window.__init__(self, x, y, width, height)
 
-    def reset_size(self):
-        y, x = self.screen.getmaxyx()
-        self._win.resize(y-1, x)
-        self._win.mvwin(0, 0)
-        self.draw()
-
-    def draw(self):
+    def redraw(self):
         y, x = self._win.getmaxyx()
-        self._win.erase()
+        self._erase()
 
-        for i, line in enumerate(self._lines[self._index:][:y]):
+        for i, line in enumerate(self._lines[self._scroll:][:y]):
             self._win.addstr(y-1-i, 0, line)
 
+        self._refresh()
+        self.redraw_cursor()
+        
     def key_event(self, key):
         if key in (curses.KEY_UP, curses.KEY_DOWN):
             if key == curses.KEY_UP:
-                self._index += 1
-            if key == curses.KEY_DOWN and self._index >= 1:
-                self._index -= 1
-            self.draw()
-            self.refresh() 
+                self._scroll += 1
+            if key == curses.KEY_DOWN and self._scroll >= 1:
+                self._scroll -= 1
+            self.redraw()
         
 @wrapper
 def run(screen, client):
-    user_input = UserInput(screen, client)
-    chat = Chat(screen, None)
-    chat._lines = ['Maxima: ça va ça va', 'Truc: oui et toi ?', 'Maxima: salut ça va ?']
-    chat.draw()
-
     screen.refresh()
-    chat.refresh()
-    user_input.refresh()
+    y, x = screen.getmaxyx()
+    user_input = UserInput(client, 0, y-1, x, 1)
+    chat = Chat([], user_input.redraw_cursor, 0, 0, x, y-1)
+
+    # Only for test
+    chat._lines = ['Maxima: ça va ça va', 'Truc: oui et toi ?', 'Maxima: salut ça va ?']
+    chat.redraw()
+    user_input.redraw()
+
     while True:
         key = screen.getch()
         chat.key_event(key)
         user_input.key_event(key)
-        
